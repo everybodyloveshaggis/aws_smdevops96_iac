7b8744a901db3c112f87e6d9f3365386171372f2
Scott McMahon <111693925+everybodyloveshaggis@users.noreply.github.com>
2026-02-21 23:58:24 +0000

    Refactor AWS key pair and instance key name
    Refactor AWS key pair resource to use variable for public key and update instance configuration.

--- initial_example/main.tf
+++ initial_example/main.tf
@@ -4,23 +4,26 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
-
   owners = ["099720109477"] # Canonical
-}
-
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/terraform_ec2.pub")
 }
 
 variable "ssh_cidr" {
   description = "CIDR allowed to SSH"
   type        = string
+}
+
+variable "public_key" {
+  description = "SSH public key"
+  type        = string
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = var.public_key
 }
 
 resource "aws_vpc" "app_vpc" {
@@ -39,7 +42,6 @@ resource "aws_subnet" "app_subnet" {
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.app_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -54,7 +56,6 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_security_group" "ssh_sg" {
   name   = "ssh-access"
   vpc_id = aws_vpc.app_vpc.id
-
   ingress {
     description = "SSH"
     from_port   = 22
@@ -62,7 +63,6 @@ resource "aws_security_group" "ssh_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.ssh_cidr]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -76,9 +76,12 @@ resource "aws_instance" "app_server" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.app_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh_sg.id]
-  key_name               = "my-key"
-
+  key_name               = aws_key_pair.my_key.key_name
   tags = {
     Name = "learn-terraform-app-server"
   }
+}
+
+output "instance_ip" {
+  value = aws_instance.app_server.public_ip
 }
