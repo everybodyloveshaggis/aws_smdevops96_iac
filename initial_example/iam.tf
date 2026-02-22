995e6dc42c1c3164a0b7fde6bd7f1eead4e4d220
SCOTT MCMAHON <everybodyloveshaggis@gmail.com>
2026-02-22 13:04:19 +0000

    fixing route table

--- initial_example/iam.tf
+++ initial_example/iam.tf
@@ -1,19 +1,47 @@
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name = "ecsTaskExecutionRole"
+# iam.tf
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-      Effect = "Allow"
-    }]
-  })
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRolePolicy" {
-  role      = aws_iam_role.ecsTaskExecutionRole.name
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_managed" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# If your ecs.tf *explicitly references* aws_iam_role_policy.ecsTaskExecution,
+# declare it (even if you mainly rely on the managed attachment above).
+resource "aws_iam_role_policy" "ecsTaskExecution" {
+  name = "ecsTaskExecutionInline"
+  role = aws_iam_role.ecsTaskExecutionRole.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
--- initial_example/route-table.tf
+++ initial_example/route-table.tf
@@ -1,12 +1,21 @@
+# route-table.tf
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.app_vpc.id
+  vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  tags = { Name = "public-rt" }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.app_subnet.id
+resource "aws_route_table_association" "public1_assoc" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public2_assoc" {
+  subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.public_rt.id
 }
