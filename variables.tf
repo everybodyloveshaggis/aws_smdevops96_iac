8b4b2a16dcf8983439d811cb7f4232c6f69f4289
SCOTT MCMAHON <everybodyloveshaggis@gmail.com>
2026-04-02 21:44:46 +0100

    dummy commit to push tf changes

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Project name prefix for resources"
   type        = string
-  default     = "smdevops96-nextjs"
+  default     = "smdevops96-nextjs-cvsite"
 }
 
 variable "environment" {
